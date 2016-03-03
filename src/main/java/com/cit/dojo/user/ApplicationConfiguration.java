e66910f6234c0516f22a216e4c83e8aa791de77e
Elisangela Dayani de Oliveira <elisangela@ciandt.com>
2016-03-03 14:24:06 -0300

    findAll Users

--- src/main/java/com/cit/dojo/user/ApplicationConfiguration.java
+++ src/main/java/com/cit/dojo/user/ApplicationConfiguration.java
@@ -1,14 +1,17 @@
 package com.cit.dojo.user;
 
 import com.cit.dojo.user.domain.UserTest;
+import jersey.repackaged.com.google.common.collect.Lists;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.PathParam;
+import java.util.ArrayList;
 
 @Configuration
 @EnableAutoConfiguration
@@ -16,14 +19,15 @@ import javax.ws.rs.PathParam;
 @RestController
 public class ApplicationConfiguration {
 
-    @RequestMapping("/users/{id}")
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET, produces = {"application/json"})
     public UserTest findById(@PathParam("id") String id) {
-        return new UserTest("xxx","xxxx");
+        return new UserTest("1", "Thor");
     }
 
-    @RequestMapping("/users")
-    public String findAll() {
-        return "Hello World ";
+    @RequestMapping(value = "/users", method = RequestMethod.GET, produces = {"application/json"})
+    public ArrayList<UserTest> findAll() {
+        return Lists.newArrayList(new UserTest("1","Thor"), new UserTest("2","Wolverine"), new UserTest("3","Hulk"));
     }
 
     public static void main(String[] args) {
